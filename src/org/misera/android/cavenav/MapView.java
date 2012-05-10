8c00130292d3e4c8314108243eff117b96d67193
T Beyer <tim.beyer@gmail.com>
2012-05-10 19:28:55 +0200

    raycaster should be orientation independent now

--- src/org/misera/android/cavenav/MapView.java
+++ src/org/misera/android/cavenav/MapView.java
@@ -170,7 +170,7 @@ public class MapView extends View {
 			
 			rayCaster.playerPos[0] = (int) Math.floor(mPosX + centerX);
 			rayCaster.playerPos[1] = (int) Math.floor(mPosY + centerY);
-			rayCaster.viewingAngle =  - heading + 90 + 180;
+			rayCaster.viewingAngle =  angle + 90;
 			
 			rayCastRenderer.invalidate();
 			
